2172092d87613f621e78896fa8464dea028ac5d9
Lauren Rose <larose@radford.edu>
2019-02-22 20:57:50 -0500

    add a loop for better comparison

--- src/TimerTest.java
+++ src/TimerTest.java
@@ -1,10 +1,13 @@
 public class TimerTest {
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
-        System.out.println(sumTwoNumbers(5, 10));
-        long endTime = System.nanoTime();
+        for (int i = 0; i < 100; i++) {
+            long startTime = System.nanoTime();
+            int sum = sumTwoNumbers(i*4, 2*i+3);
+            long endTime = System.nanoTime();
 
-        System.out.println("duration: " + (endTime - startTime) + " nanoseconds");
+            System.out.println("Sum of " + (i * 4) + " and " + (2 * i + 3) + " is " + sum);
+            System.out.println("duration: " + (endTime - startTime) + " nanoseconds\n");
+        }
     }
 
     private static int sumTwoNumbers(int a, int b) {
